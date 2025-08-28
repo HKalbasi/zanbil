001ab9757b3af28357d197c72d9ded678d4b354e
hkalbasi <hamidrezakalbasi@protonmail.com>
2025-08-28 12:21:10 +0330

    Expose bare bone zanbil crate

--- zanbil-build/src/lib.rs
+++ zanbil-build/src/lib.rs
@@ -1,8 +1,9 @@
-use std::fmt::Write;
+use std::{fmt::Write, path::PathBuf};
 
+use base64::{Engine, prelude::BASE64_URL_SAFE};
 use build_rs::input::out_dir;
 use fs_extra::dir::CopyOptions;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use toml::Value;
 
 fn dep_includes() -> Vec<(String, String)> {
@@ -19,36 +20,96 @@ fn dep_includes() -> Vec<(String, String)> {
     includes
 }
 
-#[derive(Default, Deserialize)]
-struct ZanbilConf {
-    cpp: Option<u8>,
+#[derive(Debug, Default, Deserialize)]
+pub struct ZanbilConf {
+    pub cpp: Option<u8>,
+    #[serde(default)]
+    pub make_dependencies_public: bool,
 }
 
-pub fn build() {
-    let mut cc = cc::Build::new();
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Dependency {
+    pub include_dirs: Vec<PathBuf>,
+}
 
-    let my_name = build_rs::input::cargo_manifest_links().expect("zanbil expects a link name");
+#[derive(Debug)]
+pub struct ZanbilCrate {
+    pub name: String,
+    pub config: ZanbilConf,
+    pub include_dir: PathBuf,
+    pub aggregated_include_dirs: Vec<PathBuf>,
+    pub dependencies: Vec<Dependency>,
+}
+
+pub fn init_zanbil_crate() -> ZanbilCrate {
+    let name = build_rs::input::cargo_manifest_links().expect("zanbil expects a link name");
+
+    let cargo_toml_path = build_rs::input::cargo_manifest_dir().join("Cargo.toml");
+    build_rs::output::rerun_if_changed(&cargo_toml_path);
+    let cargo_toml = std::fs::read_to_string(cargo_toml_path).unwrap();
+
+    let value: Value = toml::from_str(&cargo_toml).unwrap();
+
+    let config: ZanbilConf = value
+        .get("package")
+        .and_then(|x| x.get("metadata")?.get("zanbil")?.clone().try_into().ok())
+        .unwrap_or_default();
+
+    let include_dir = out_dir().join("include");
+    let mut dependencies: Vec<Dependency> = vec![];
+    std::fs::create_dir_all(&include_dir).unwrap();
+    std::fs::remove_dir_all(&include_dir).unwrap();
+    std::fs::create_dir_all(&include_dir).unwrap();
 
     let mut main_rs_file = String::new();
 
     for (dep, include) in dep_includes() {
         writeln!(main_rs_file, "extern crate {};", dep.to_lowercase()).unwrap();
-        cc.include(include);
+        dependencies.push(toml::from_slice(&BASE64_URL_SAFE.decode(&include).unwrap()).unwrap());
     }
+
+    let mut aggregated_include_dirs: Vec<PathBuf> = dependencies
+        .iter()
+        .flat_map(|x| &x.include_dirs)
+        .chain([&include_dir])
+        .cloned()
+        .collect();
+
+    aggregated_include_dirs.sort();
+    aggregated_include_dirs.dedup();
 
     std::fs::write(out_dir().join("generated_lib.rs"), main_rs_file).unwrap();
 
-    let cargo_toml =
-        std::fs::read_to_string(build_rs::input::cargo_manifest_dir().join("Cargo.toml")).unwrap();
+    let me = Dependency {
+        include_dirs: if config.make_dependencies_public {
+            aggregated_include_dirs.clone()
+        } else {
+            vec![include_dir.clone()]
+        },
+    };
 
-    let value: Value = toml::from_str(&cargo_toml).unwrap();
+    build_rs::output::metadata(
+        "ZANBIL_INCLUDE",
+        &BASE64_URL_SAFE.encode(toml::to_string(&me).unwrap()),
+    );
 
-    let zanbil_conf: ZanbilConf = value
-        .get("package")
-        .and_then(|x| x.get("metadata")?.get("zanbil")?.clone().try_into().ok())
-        .unwrap_or_default();
+    ZanbilCrate {
+        name,
+        config,
+        include_dir,
+        dependencies,
+        aggregated_include_dirs,
+    }
+}
 
-    let cpp = zanbil_conf.cpp;
+pub fn build() {
+    let zc = init_zanbil_crate();
+
+    let mut cc = cc::Build::new();
+
+    cc.includes(&zc.aggregated_include_dirs);
+
+    let cpp = zc.config.cpp;
 
     build_rs::output::rerun_if_env_changed("ZANBIL_CXX");
     build_rs::output::rerun_if_env_changed("ZANBIL_CC");
@@ -84,10 +145,7 @@ pub fn build() {
         }
     }
 
-    let my_include = out_dir().join("include").join(&my_name);
-    std::fs::create_dir_all(&my_include).unwrap();
-    std::fs::remove_dir_all(&my_include).unwrap();
-    std::fs::create_dir_all(&my_include).unwrap();
+    let my_include = zc.include_dir.join(&zc.name);
 
     fs_extra::dir::copy(
         "src/",
@@ -98,8 +156,4 @@ pub fn build() {
 
     cc.compile("main");
     build_rs::output::rustc_link_lib("main");
-    build_rs::output::metadata(
-        "ZANBIL_INCLUDE",
-        &my_include.parent().unwrap().to_string_lossy(),
-    );
 }
